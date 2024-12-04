e62f67190551f4f951fc1ee00aff29df3cd5a7f4
trunglk-ibl <trunglk@blockchainlabs.asia>
2024-12-04 15:47:38 +0700

    fix load image on github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
-
+const isDev = process.env.NODE_ENV === 'development';
+const repoName = 'nextjs-app-router';
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: isDev ? '' : `/${repoName}`,
+  assetPrefix: isDev ? '' : `/${repoName}`,
 };
 
 export default nextConfig;
