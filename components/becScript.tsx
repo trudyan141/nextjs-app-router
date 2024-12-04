3db2bdaf4a55ac927b5d6eb6ce92f0ffc362159d
trunglk-ibl <trunglk@blockchainlabs.asia>
2024-12-04 15:35:11 +0700

    update env theo params url

--- components/becScript.tsx
+++ components/becScript.tsx
@@ -1,10 +1,20 @@
 'use client';
 import Script from "next/script";
+import { useState , useEffect} from "react";
 declare const TE;
 
 export default function BecScript({
-
 }) {
+  const [env, setEnv] = useState<string | null>(null);
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const envParam = urlParams.get("env") || "prod";
+    setEnv(envParam);
+  }, []);
+   const scriptSrc =
+    env === "dev"
+      ? "https://tma-demo.dmtp.tech/sdk/0.0.8/bec.js?walletAddress=QnLOYksIDhA3MfBLoRL%2ByIa8jRggeovB3NtN3d7LD7g%3D"
+      : "https://bec.dmtp.tech/0.0.8/bec.js?walletAddress=QnLOYksIDhA3MfBLoRL%2ByIa8jRggeovB3NtN3d7LD7g%3D";
   return (
     <>
      <Script
@@ -14,17 +24,17 @@ export default function BecScript({
          console.log("Telegram Web App loaded");
         }}
       />
-     <Script
-        src="https://tma-demo.dmtp.tech/sdk/0.0.8/bec.js?walletAddress=39oXQfuzAVeV2QaqYzmAWC8w9sgl7Bvvhu%2FnrBf2bYw%3D"
+      {env && <Script
+        src={scriptSrc}
         strategy="afterInteractive" // Loads the script after the page is interactive
         onLoad={() => {
           if (typeof TE.onLoad === "function") {
-              TE.onLoad();
+            TE.onLoad();
           } else {
             console.error('TE.onLoad is not a function');
           }
         }}
-      />
+      />}
     </>
   );
 }
