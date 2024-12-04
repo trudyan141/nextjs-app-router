36076acb125b22f327ee75bb653d5f7acfe40812
trunglk-ibl <trunglk@blockchainlabs.asia>
2024-12-04 15:57:42 +0700

    test images

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
-const isDev = process.env.NODE_ENV === 'development';
-const repoName = 'nextjs-app-router';
+
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: isDev ? '' : `/${repoName}`,
-  assetPrefix: isDev ? '' : `/${repoName}`,
-  images: {
-    unoptimized: true, 
-  },
-  trailingSlash: true,
 };
 
 export default nextConfig;
